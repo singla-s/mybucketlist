90ed145614c9981a57ab8775e01aeb1ee3a3ecad
carpdm-dev <sinnisingla@outlook.com>
2020-10-20 22:40:37 +0530

    Complete the initial configuration at backend and front end

--- app.js
+++ app.js
@@ -6,15 +6,24 @@ var bodyParser = require("body-parser");
 //Include body parser to read requests
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Use EJS
+app.set('view engine', 'ejs');
 //load stactic files
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname + "style"));
+// app.use(express.static(__dirname + "image"));
+//////////////////////////Main////////////////////////////////////
+
+
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var date = new Date();
+    var dateToday = date.toLocaleDateString('en-IN', options);
+    res.render("bucket-list",{dayName : dateToday});
 });
 
 
 // Enable Port
-app.listen(process.env.PORT || 3000, function() {
+app.listen(3000, function() {
     console.log("Server listening on port 3000");
 });
